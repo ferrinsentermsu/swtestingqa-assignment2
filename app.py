c5f910adc084f742cd11fdcb33ad3ed5212201e0
Ferrin Senter <fas96@msstate.edu>
2022-04-07 15:52:46 -0500

    web dev

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 def calculate():
     bmi = ''
+    category = ""
     if request.method=='POST' and 'userFeet' in request.form and 'userInches' in request.form and 'userPounds':
         ft=float(request.form.get('userFeet'))
         inch=float(request.form.get('userInches'))
@@ -19,10 +20,10 @@ def calculate():
         bmi = float("{:.1f}".format(weight / totalinches))
 
         # CLASSIFICATION
-        category = ""
-        if(bmi <= 18.5):
+        
+        if(bmi < 18.5):
             category = "underweight"
-        elif(bmi > 18.4 and bmi <= 24.9):
+        elif(bmi >= 18.5 and bmi <= 24.9):
             category = "normal"
         elif(bmi >= 25 and bmi <= 29.9):
             category = "overweight"
